5afa5bb9030b94d015ab3d5049a7251c551f2886
Yohann Paris <github@yohannparis.com>
2024-04-11 09:26:30 -0400

    Remove folder not needed

--- api_use_examples/dataset_dkg/dataset_dkg_example.py
+++
@@ -1,9 +0,0 @@
-from mitaskem.src import connect
-import os
-
-if __name__ == "__main__":
-    with open("./data_sample/covid_deaths_usafacts.csv", "r") as f:
-        csv = f.read()
-    GPT_KEY = os.environ.get('OPENAI_API_KEY')
-    res, yes = connect.dataset_header_dkg(csv, GPT_KEY)
-    print(res)
